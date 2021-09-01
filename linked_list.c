02cc9085658417197348da86923132cffeca601b
Manoj Eswaran <manojeswaran@Manojs-MacBook-Air.local>
2021-09-01 21:03:04 +0530

    comments added and added common function for new_node creation

--- linked_list.c
+++ linked_list.c
@@ -3,100 +3,118 @@
 
 struct list
 {
-   int data;
-   struct list *next;
+	int data;
+	struct list *next;
 };
 
-void insert (struct list **head, int value)
+struct list * new_node (int value)
 {
-  struct list *temp = NULL;
-  struct list *current = *head;
+	struct list *temp = NULL;
 
-  temp = (struct list *)malloc (sizeof(struct list));
-  temp->data = value;
-  temp->next = NULL; 
- 
-  if (*head == NULL)
-  {
-	  *head = temp;
-	  return;
-  }
-
-  while(current->next != NULL) {
-     current = current->next;
-  } 
+	temp = (struct list *) malloc (sizeof(struct list));
+	temp->data = value;
+	temp->next = NULL; 
+  
+	return temp;
+}
    
-  current->next = temp;
-  return;
+
+void insert_tail (struct list **head, int value)
+{
+	struct list *temp = NULL;
+	struct list *current = *head;
+
+        temp = new_node(value);
+
+	if (*head == NULL)
+	{
+		*head = temp;
+		return;
+	}
+
+	while(current->next != NULL) {
+		current = current->next;
+	} 
+
+	current->next = temp;
+	return;
 }
 
 void insert_beginning (struct list **head, int value)
 {
 
-  struct list *temp = NULL;
-  struct list *current = *head;
+	struct list *temp = NULL;
+	struct list *current = *head;
 
-  temp = (struct list *) malloc (sizeof(struct list));
-  temp->data = value;
-  temp->next = current;
+	temp = (struct list *) malloc (sizeof(struct list));
+	temp->data = value;
+	temp->next = current;
 
-  *head = temp;
+	*head = temp;
 
 }
 
 void insert_between (struct list **head, int value, int pos)
 {
 
-  struct list *temp = NULL;
-  struct list *current = *head;
-  int local_count = 1;
+	struct list *temp = NULL;
+	struct list *current = *head;
+	int local_count = 1;
 
-  temp = (struct list *) malloc (sizeof(struct list));
-  temp->data = value;
-  temp->next = NULL;
+	temp = (struct list *) malloc (sizeof(struct list));
+	temp->data = value;
+	temp->next = NULL;
 
-  if ( pos == local_count )
-  {
-    insert_beginning(head,value);
-  } else {
-    while((current!=NULL) && (pos!=local_count+1))
-    {
-      current = current->next;
-      local_count = local_count + 1;
-    }
-    
-    temp->next = current->next;
-    current->next = temp;
-  }
-    
+	if ( pos == local_count )
+	{
+		insert_beginning(head,value);
+	} else {
+		while((current!=NULL) && (pos!=local_count+1))
+		{
+			current = current->next;
+			local_count = local_count + 1;
+		}
+
+		temp->next = current->next;
+		current->next = temp;
+	}
+
 
 }
 
 void print_list (struct list* head)
 {
-  while(head != NULL)
-  {
-    printf("%d   >",head->data);
-    head = head->next;
-  } 
+	while(head != NULL && head->next!= NULL)
+	{
+		printf("%d   >",head->data);
+		head = head->next;
+	} 
+	printf("%d\n",head->data);
 
 }
 
 int main()
 {
 
-  struct list *head = NULL;
+	struct list *head = NULL;
 
-  insert(&head, 10);
-  insert(&head, 11);
-  insert(&head, 12);
-  insert(&head, 14);
-  insert(&head, 15);
-  insert_beginning(&head, 9);
-  insert_beginning(&head, 8);
-  insert_between(&head,13,5);
+	// insert at tail
+	insert_tail(&head, 10);
+	insert_tail(&head, 11);
+	insert_tail(&head, 12);
+	insert_tail(&head, 14);
+	insert_tail(&head, 15);
 
-  print_list(head);
+	// insert at head
+	insert_beginning(&head, 9);
+	insert_beginning(&head, 8);
 
-return 0;
+	// insert at specific position
+	insert_between(&head,13,5);
+	insert_between(&head,1,1);
+	insert_between(&head,2,2);
+
+	print_list(head);
+
+	return 0;
 }
